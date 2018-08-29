7c8c415b54e72446d06312bf5385ab3c5afad855
Angela Duran <duranangela19@gmail.com>
2018-08-29 10:21:11 -0600

    Still working on controller - stuck on JSON

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,16 @@
 class SearchController < ApplicationController
 
   def index
-    zip = params[:zipcode]
-    @conn = Faraday.new(https://developer.nrel.gov/api/alt-fuel-stations/v1) do |faraday|
-      faraday.headers["api-key"] = "FVyyBmXK03QnpuDtmgiIlcDQ2r6USMXO1dcvYyVE"
+    zip = params[:q]
+    @conn = Faraday.new(url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1') do |faraday|
+      faraday.headers["api_key"] = "FVyyBmXK03QnpuDtmgiIlcDQ2r6USMXO1dcvYyVE"
       faraday.adapter Faraday.default_adapter
     end
 
-    response = @conn.get("/nearest.json?location=#{zip}&fuel_type=LPG,ELEC&radius=6.0&limit=10")
+    response = @conn.get("/nearest.json?location=#{zip}&fuel_type=LPG,ELEC&radius=6.0&limit=10&format=JSON&api_key=FVyyBmXK03QnpuDtmgiIlcDQ2r6USMXO1dcvYyVE")
 
+    binding.pry
+    @stations = JSON.parse(response.body, symbolize_names: true)
   end
 
 end
