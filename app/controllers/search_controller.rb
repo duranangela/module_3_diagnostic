4e076fbcf698b36139bf79ce2923fcb822a89a6c
Angela Duran <duranangela19@gmail.com>
2018-08-29 10:02:11 -0600

    Working on search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,13 @@
 class SearchController < ApplicationController
 
   def index
+    zip = params[:zipcode]
+    @conn = Faraday.new(https://developer.nrel.gov/api/alt-fuel-stations/v1) do |faraday|
+      faraday.headers["api-key"] = "FVyyBmXK03QnpuDtmgiIlcDQ2r6USMXO1dcvYyVE"
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = @conn.get("/nearest.json?location=#{zip}&fuel_type=LPG,ELEC&radius=6.0&limit=10")
 
   end
 
