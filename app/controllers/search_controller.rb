0f954fe3db3e24830c94c9b078264867d51f1442
Angela Duran <duranangela19@gmail.com>
2018-08-29 10:28:33 -0600

    Still stuck

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,14 +2,14 @@ class SearchController < ApplicationController
 
   def index
     zip = params[:q]
-    @conn = Faraday.new(url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1') do |faraday|
-      faraday.headers["api_key"] = "FVyyBmXK03QnpuDtmgiIlcDQ2r6USMXO1dcvYyVE"
+    conn = Faraday.new(url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1') do |faraday|
+    #   faraday.headers["api_key"] = "FVyyBmXK03QnpuDtmgiIlcDQ2r6USMXO1dcvYyVE"
       faraday.adapter Faraday.default_adapter
     end
 
-    response = @conn.get("/nearest.json?location=#{zip}&fuel_type=LPG,ELEC&radius=6.0&limit=10&format=JSON&api_key=FVyyBmXK03QnpuDtmgiIlcDQ2r6USMXO1dcvYyVE")
+    response = conn.get("/nearest.json?location=#{zip}&fuel_type=LPG,ELEC&radius=6.0&limit=10&api_key=FVyyBmXK03QnpuDtmgiIlcDQ2r6USMXO1dcvYyVE")
 
-    binding.pry
+
     @stations = JSON.parse(response.body, symbolize_names: true)
   end
 
