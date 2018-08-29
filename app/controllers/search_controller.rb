bf3723c8b553f784743557d225c0c1733681f176
Angela Duran <duranangela19@gmail.com>
2018-08-29 11:00:42 -0600

    Clean up stuff

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,12 +2,8 @@ class SearchController < ApplicationController
 
   def index
     zip = params[:q]
-    # conn = Faraday.new(url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1') do |faraday|
-    # #   faraday.headers["api_key"] = "FVyyBmXK03QnpuDtmgiIlcDQ2r6USMXO1dcvYyVE"
-    #   faraday.adapter Faraday.default_adapter
-    # end
-    # binding.pry
-    response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&fuel_type=LPG,ELEC&radius=6.0&limit=10&api_key=FVyyBmXK03QnpuDtmgiIlcDQ2r6USMXO1dcvYyVE")
+
+    response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{zip}&fuel_type=LPG,ELEC&radius=6.0&limit=10&api_key=FVyyBmXK03QnpuDtmgiIlcDQ2r6USMXO1dcvYyVE")
 
     @stations = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
   end
